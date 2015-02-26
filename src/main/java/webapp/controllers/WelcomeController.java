10a2e83367f74222d7eeec665666d25764323642
Ankit Vani <a@nevitus.org>
2015-02-26 15:18:42 +0530

    Remove the temporary JPA entity to prepare for real entity generation

--- src/main/java/webapp/controllers/WelcomeController.java
+++ src/main/java/webapp/controllers/WelcomeController.java
@@ -1,21 +1,15 @@
 package webapp.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import webapp.model.repositories.UserRepository;
-
 @Controller
 public class WelcomeController {
 
-	@Autowired
-	UserRepository userRepository;
-
 	@RequestMapping("/")
 	public String welcome(Model model) {
-		model.addAttribute("displayString", userRepository.findByName("Ankit Vani").get(0).getName());
+		model.addAttribute("displayString", "Hey world, sup?");
 		return "welcome";
 	}
 }
