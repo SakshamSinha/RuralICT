dafa63b2af5a961ee491e9366e8aca6a4e150fad
Ankit Vani <a@nevitus.org>
2015-03-28 18:57:54 +0530

    Enable caching

--- src/main/java/webapp/RuralIvrsApplication.java
+++ src/main/java/webapp/RuralIvrsApplication.java
@@ -2,8 +2,10 @@ package webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class RuralIvrsApplication {
 
     public static void main(String[] args) {
