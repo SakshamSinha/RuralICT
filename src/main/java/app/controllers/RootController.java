fff4f4c21918b3e5207e97bf4aa7dc5b430f9151
Ankit Vani <a@nevitus.org>
2015-04-01 10:53:08 +0530

    If the user is in only one org, forward them to that org

--- src/main/java/app/controllers/RootController.java
+++ src/main/java/app/controllers/RootController.java
@@ -1,12 +1,17 @@
 package app.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import app.entities.Organization;
+import app.entities.OrganizationMembership;
 import app.entities.User;
 import app.rest.repositories.OrganizationRepository;
 import app.rest.repositories.UserRepository;
@@ -27,10 +32,30 @@ public class RootController {
 	}
 
 	@RequestMapping("/web")
+	@Transactional
 	public String webRoot(Model model) {
 		User user = Utils.getCurrentUser(userRepository);
-		model.addAttribute("organizationMemberships", user.getOrganizationMemberships());
-		return "choose"; // TODO we need to implement this template
+		List<OrganizationMembership> authenticated = new ArrayList<OrganizationMembership>();
+
+		/*
+		 * Right now, we're only letting admins access the web interface. So only take into consideration orgs
+		 * that the user is an admin of.
+		 */
+		for (OrganizationMembership m : user.getOrganizationMemberships()) {
+			if (m.getIsAdmin())
+				authenticated.add(m);
+		}
+
+		/*
+		 * If the user is an admin in only one org, forward them to that org, else
+		 * show them a list of orgs they can access.
+		 */
+		if (authenticated.size() == 1) {
+			return "redirect:/web/"+authenticated.get(0).getOrganization().getAbbreviation()+"/";
+		} else {
+			model.addAttribute("organizationMemberships", authenticated);
+			return "choose"; // TODO we need to implement this template
+		}
 	}
 
 	@RequestMapping(value="/web/{org}/")
