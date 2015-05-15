8fd641b5538f94d584e475880e94bc846596a2c3
shahpurnima <shahpurnima2@gmail.com>
2015-05-15 16:26:59 +0530

    groups add functionlity

--- src/main/resources/static/js/groupOps.js
+++ src/main/resources/static/js/groupOps.js
@@ -1,13 +1,43 @@
 /**
  * This contains the group operations such as, add new, sort, etc.
  */
+website.factory("Group", function($resource) {
+	return $resource("/app/api/groups/:id");
+});
+
+
+website.controller("GroupsCtrl", function($scope, $route, Group) {
+
+	$scope.saveGroup = function(data) {
+		var group = new Group(data);
+		group.organization = "organizations/" + $("#organization").attr("data-orgId");
+		//group.parentGroup = "groups/1";
+		group.$save(function(group) {
+		 	
+			window.location.reload(true);
+		    $scope.groupName = group.name;
+			$("#group-add-success-modal").modal('toggle');
+			
+		}, function(error) {
+			$scope.failure = error.data;
+			$("#group-add-failed-modal").modal('toggle');
+		});
+	}
+
+});	   
+	
 
 //add new group from index.html
 $("#add-new-group").click(function() {  
-    var groupName = $.trim($('#new-group-name-input').val());
-    var ul = document.getElementById("group-names");
-	var li = document.createElement("li");
-	var new_group_li = $("<a href=\"#/groups\">"+groupName+"</a>");
+    var name = $.trim($('#new-group-name-input').val());
+    console.log(name);
+    var data ={};
+    data.name=name;
+    
+    angular.element($('#add-new-group')).scope().saveGroup(data);
+    
+    $('#add-new-group-modal').modal('toggle');
+
+    $('#new-group-name-input').val("");
 	
-	new_group_li.appendTo(ul);
 });
