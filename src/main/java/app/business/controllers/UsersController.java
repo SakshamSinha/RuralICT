83a1837e85f2782cf131d85506df7de00b4b4731
Amey Kamat <ask5332@gmail.com>
2015-05-26 15:35:24 +0530

    Resolving bugs in controller

--- src/main/java/app/business/controllers/UsersController.java
+++ src/main/java/app/business/controllers/UsersController.java
@@ -6,13 +6,13 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import app.business.services.UserViewService;
-import app.business.services.UserViewService.UserDetails;
 
 @Controller
 @RequestMapping("/web/{org}")
@@ -26,7 +26,7 @@ public class UsersController {
 	@Transactional
 	public String usersPage(@PathVariable String org, Model model) {
 
-		List<UserDetails> rows = userViewService.getUserDetailsByOrganization(org);
+		List<UserView> rows = userViewService.getUserViewByOrganization(org);
 		
 		model.addAttribute("organizationMemberships",rows);
 		return "users";
