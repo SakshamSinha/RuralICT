628d832fdf7ebad25b02613961c027b492ebb2bb
kranthi <kranthi-coder@users.noreply.github.com>
2015-05-27 13:41:04 +0530

    modified inboundcall, outboundcall, voice services and broadcastrecipient, inboundcall and voice repos

--- src/main/java/app/business/controllers/UsersController.java
+++ src/main/java/app/business/controllers/UsersController.java
@@ -6,13 +6,13 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import app.business.services.UserViewService;
+import app.business.services.UserViewService.UserView;
 
 @Controller
 @RequestMapping("/web/{org}")
---
+++ src/main/java/app/business/services/InboundCallService.java
@@ -0,0 +1,37 @@
+package app.business.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import app.data.repositories.InboundCallRepository;
+import app.entities.Organization;
+import app.entities.InboundCall;
+import app.entities.Group;
+
+@Service
+public class InboundCallService {
+	@Autowired
+	InboundCallRepository inboundCallRepository;
+	
+	
+	public List<InboundCall> getInboundCallList(Organization organization){
+	 return inboundCallRepository.findByOrganization(organization);
+	}
+	
+	public List<InboundCall> getInboundCallList(Group group){
+		//************************************************
+		return null;
+	}
+	
+	public void addInboundCall(InboundCall inboundCall){
+		inboundCallRepository.save(inboundCall);
+	}
+	
+	public void removeInboundCall(InboundCall inboundCall){
+		inboundCallRepository.delete(inboundCall);
+	}
+	
+}
