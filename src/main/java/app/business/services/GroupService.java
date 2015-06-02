6be2699493c7369d933e01e9733065804064361c
Amey Kamat <ask5332@gmail.com>
2015-06-02 17:13:59 +0530

    Adding @Service annotation to all services

--- src/main/java/app/business/services/GroupService.java
+++ src/main/java/app/business/services/GroupService.java
@@ -3,10 +3,12 @@ package app.business.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import app.data.repositories.GroupRepository;
 import app.entities.Group;
 
+@Service
 public class GroupService {
 	@Autowired
 	GroupRepository groupRepository;
