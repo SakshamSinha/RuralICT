9e876ab5dabb62e6e253839a5cdf596f6a2028ca
shahpurnima <shahpurnima2@gmail.com>
2015-06-03 20:13:58 +0530

    view part for voice or text broadcast (database fetching is remaining for user )and view part for show group wise meember(member list is fetching from database)

---
+++ src/main/java/app/business/controllers/MemberListController.java
@@ -0,0 +1,83 @@
+package app.business.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+
+import app.data.repositories.GroupRepository;
+import app.data.repositories.UserPhoneNumberRepository;
+import app.entities.Group;
+import app.entities.GroupMembership;
+import app.entities.User;
+import app.entities.UserPhoneNumber;
+import app.entities.broadcast.Broadcast;
+
+@Controller
+@RequestMapping("/web/{org}")
+public class MemberListController {
+
+	@Autowired
+	GroupRepository groupRepository;
+
+	@Autowired
+	UserPhoneNumberRepository userPhoneNumberRepository;
+
+
+	static private class UserRow {
+		private User user;
+		private UserPhoneNumber phone;
+
+		public UserRow(User user, UserPhoneNumber primaryPhoneNo ) {
+			this.user = user;
+			this.phone = primaryPhoneNo;
+
+		}
+		public User getUser() {
+			return user;
+		}
+		public void setUser(User user) {
+			this.user = user;
+		}
+		public UserPhoneNumber getPhone() {
+			return phone;
+		}
+		public void setPhone(UserPhoneNumber phone) {
+			this.phone = phone;
+
+		}
+	}
+
+
+
+	@RequestMapping(value="/memberList/{groupId}")
+	@PreAuthorize("hasRole('ADMIN'+#org)")
+	@Transactional
+	public String memberList(@PathVariable String org, @PathVariable int groupId, Model model) {
+
+		Group group = groupRepository.findOne(groupId);
+		List<UserRow> rows = new ArrayList<UserRow>();
+		List<GroupMembership> groupMemberShipList = group.getGroupMemberships();
+		for(GroupMembership memberShip : groupMemberShipList)
+		{
+			UserPhoneNumber users = userPhoneNumberRepository.findByUserAndPrimaryTrue(memberShip.getUser());
+			UserRow row = new UserRow(memberShip.getUser(), users);
+			rows.add(row);
+
+		}
+		model.addAttribute("groupWiseMemberShip",rows);
+
+		return "groupWiseMember";
+	}
+
+}
