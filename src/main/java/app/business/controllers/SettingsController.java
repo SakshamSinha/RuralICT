0c1c50f0eac718954f57d48de47b9e5b1ba96976
Pranav Ganorkar <pranavg189@gmail.com>
2015-06-11 11:19:04 +0530

    Modified settings controller

--- src/main/java/app/business/controllers/SettingsController.java
+++ src/main/java/app/business/controllers/SettingsController.java
@@ -1,19 +1,31 @@
 package app.business.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import app.data.repositories.GroupRepository;
+import app.data.repositories.OrganizationRepository;
+import app.entities.Group;
+import app.entities.Organization;
 
 @Controller
 @RequestMapping("/web/{org}")
 public class SettingsController {
+	
+	@Autowired
+	OrganizationRepository organizationRepository;
 
 	@RequestMapping(value="/settingsPage")
 	@PreAuthorize("hasRole('ADMIN'+#org)")
 	public String settingsPage(@PathVariable String org, Model model) {
+		Organization organization = organizationRepository.findByAbbreviation(org);
+		model.addAttribute("organization", organization);
 		return "settings";
 	}
-
+	
 }
