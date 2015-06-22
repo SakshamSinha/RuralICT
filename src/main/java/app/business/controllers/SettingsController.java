0cb906ee70fccb42a4894353148e04f096c75b4d
Pranav Ganorkar <pranavg189@gmail.com>
2015-06-23 01:01:09 +0530

    Added Select Language in Welcome Language Settings

--- src/main/java/app/business/controllers/SettingsController.java
+++ src/main/java/app/business/controllers/SettingsController.java
@@ -22,7 +22,11 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import app.business.services.OrganizationService;
+import app.business.services.VoiceService;
+import app.business.services.WelcomeMessageService;
 import app.entities.Organization;
+import app.entities.Voice;
+import app.entities.WelcomeMessage;
 
 @Controller
 @RequestMapping("/web/{org}")
@@ -30,6 +34,12 @@ public class SettingsController {
 	
 	@Autowired
 	OrganizationService organizationService;
+	
+	@Autowired
+	WelcomeMessageService welcomeMessageService;
+	
+	@Autowired
+	VoiceService voiceService;
 
 	@RequestMapping(value="/settingsPage")
 	@PreAuthorize("hasRole('ADMIN'+#org)")
@@ -41,30 +51,72 @@ public class SettingsController {
 	
 	@RequestMapping(value="/upload/welcomeMessage", method=RequestMethod.POST)
 	@ResponseBody
-    public void handleFileUpload(HttpServletRequest request){
+	public void handleFileUpload(HttpServletRequest request){
+		
+		// Convert for getting the files
 		MultipartHttpServletRequest mRequest;
-	    mRequest = (MultipartHttpServletRequest) request;
-	    
-	    Iterator<String> itr = mRequest.getFileNames();
-	    while (itr.hasNext()) {
-	        //org.springframework.web.multipart.MultipartFile
-	        MultipartFile mFile = mRequest.getFile(itr.next());
-	        String fileName = mFile.getOriginalFilename();
-	        System.out.println("*****"+ fileName);
-	        String workingDir = System.getProperty("user.dir");
-	 	    System.out.println("Current working directory : " + workingDir + "/"+ fileName);
-
-	        //To copy the file to a specific location in machine.
-	        File file = new File(workingDir + "/" + fileName);
-	        try {
+		mRequest = (MultipartHttpServletRequest) request;
+		
+		// Get Parameters passed from AngularJS using FormData
+		int organizationid = Integer.parseInt(request.getParameter("orgid"));
+		System.out.println("organization id is: " + organizationid);
+		Organization organization = organizationService.getOrganizationById(organizationid);
+		
+		String locale = request.getParameter("locale");
+		System.out.println("locale is: " + locale);
+		
+		// Create Required Entity Objects
+		WelcomeMessage welcomeMessage = welcomeMessageService.getbyOrganizationAndLocale(organization, locale);
+		
+		Voice voice = welcomeMessageService.getVoice(welcomeMessage);
+		
+		System.out.println("Got the Voice Object");
+		System.out.println("Now Uploading the File");
+		
+		// Upload the file to the given location
+		Iterator<String> itr = mRequest.getFileNames();
+		while (itr.hasNext()) {
+			//org.springframework.web.multipart.MultipartFile
+			MultipartFile mFile = mRequest.getFile(itr.next());
+			String fileName = mFile.getOriginalFilename();
+			System.out.println("*****"+ fileName);
+			String workingDir = System.getProperty("user.dir");
+			System.out.println("Current working directory : " + workingDir + "/"+ fileName);
+			 
+			String filePath = workingDir + "/" + fileName;
+			//To copy the file to a specific location in machine.
+			File file = new File(filePath);
+			try {
 				FileCopyUtils.copy(mFile.getBytes(), file);
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			} //This will copy the file to the specific location.
-	    }
+			
+			//Update the Voice Table
+			System.out.println("File was successfully uploaded.");
+			System.out.println("Now, updating the Voice Table");
+			
+			// Set the url of the uploaded file in voice table
+			voice.setUrl(filePath);
+			
+			// Finally, Update the voice object in the database
+			voiceService.addVoice(voice);
+			
+			System.out.println("Voice ID is "+voice.getVoiceId());
+			System.out.println("Voice URL is " + voice.getUrl());
+			
+			System.out.println("File URL was updated Successfully in the database");
+			
+			/*
+			 * TODO: What if the organzation does not have entries in the voice table and Welcome Message Table
+			 *  We will have to create them first
+			 */
+			
+			
+		}
 	}
-    
+	
 	
 
 }
--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -2,214 +2,240 @@
  *  Javascript file for the Settings Controller
  */
 website.factory("Organization", function($resource) {
-    return $resource("/api/organizations/:id", {
-        id: '@id'
-    }, {
-        query: {
-            method: "GET",
-            isArray: false
-        },
-        update: {
-            method: "PATCH",
-            params: {
-                id: '@id'
-            }
-        }
-    });
+	return $resource("/api/organizations/:id", {
+		id: '@id'
+	}, {
+		query: {
+			method: "GET",
+			isArray: false
+		},
+		update: {
+			method: "PATCH",
+			params: {
+				id: '@id'
+			}
+		}
+	});
 });
 
 website.factory("OutboundCall", function($resource) {
-    return $resource("/api/broadcastDefaultSettings/:id", {
-        id: '@id'
-    }, {
-        query: {
-            method: "GET",
-            isArray: false
-        },
-        update: {
-            method: "PATCH",
-            params: {
-                id: '@id'
-            }
-        }
-    });
+	return $resource("/api/broadcastDefaultSettings/:id", {
+		id: '@id'
+	}, {
+		query: {
+			method: "GET",
+			isArray: false
+		},
+		update: {
+			method: "PATCH",
+			params: {
+				id: '@id'
+			}
+		}
+	});
 });
 
+
+
 website.directive('fileModel', ['$parse',function ($parse) {
-    return {
-        restrict: 'A',
-        link: function(scope, element, attrs) {
-            var model = $parse(attrs.fileModel);
-            var modelSetter = model.assign;
-            
-            element.bind('change', function(){
-                scope.$apply(function(){
-                    modelSetter(scope, element[0].files[0]);
-                });
-            });
-        }
-    };
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			var model = $parse(attrs.fileModel);
+			var modelSetter = model.assign;
+			
+			element.bind('change', function(){
+				scope.$apply(function(){
+					modelSetter(scope, element[0].files[0]);
+				});
+			});
+		}
+	};
 }]);
 
 website.controller("SettingsCtrl", function($scope,$http,$routeParams, Organization, OutboundCall) {
 
-    // get the current organization id
-    var orgid = document.getElementById("settings-page").getAttribute("orgid");
-    var outboundcallid = document.getElementById("settings-page").getAttribute("orgid");
+	// get the current organization id
+	var orgid = document.getElementById("settings-page").getAttribute("orgid");
+	var outboundcallid = document.getElementById("settings-page").getAttribute("orgid");
 
-    $scope.selectOptions = [{
-        name: 'Disable(बंद करना)',
-        value: '0'
-    }, {
-        name: 'Enable(चालू करना)',
-        value: '1'
-    }];
-    
-    $scope.incomingCheckBoxOptions = {
-    		"order" : false,
-            "feedback" : false,
-            "response" : false
-    };
-    $scope.outgoingCheckBoxOptions = {
-    		"order" : false,
-    		"feedback" : false,
-    		"response" : false
-    };    
+	$scope.selectOptions = [{
+		name: 'Disable(बंद करना)',
+		value: '0'
+	}, {
+		name: 'Enable(चालू करना)',
+		value: '1'
+	}];
+	
+	$scope.languageOptions = [{
+		language: 'English',
+		locale: 'en',
+		value: '0'
+	}, {
+		language: 'Marathi',
+		locale: 'mr',
+		value: '1'
+	}, {
+		language: 'Hindi',
+		locale: 'hi',
+		value: '2'
+	}];
+		
+	
+	$scope.incomingCheckBoxOptions = {
+			"order" : false,
+			"feedback" : false,
+			"response" : false
+	};
+	$scope.outgoingCheckBoxOptions = {
+			"order" : false,
+			"feedback" : false,
+			"response" : false
+	};    
 
-    var organization = Organization.get({
-        id: orgid
-    }, function() {
+	var organization = Organization.get({
+		id: orgid
+	}, function() {
 
-        /*
-         *  set initial values for 'select' element model from database
-         */
+		/*
+		 *  set initial values for 'select' element model from database
+		 */
 
-        // 'select' elements from dashboard options
-        $scope.feedbackSelect = $scope.selectOptions[Number(organization.enableFeedbacks)].value;
-        $scope.responseSelect = $scope.selectOptions[Number(organization.enableResponses)].value;
-        $scope.billSelect = $scope.selectOptions[Number(organization.enableBilling)].value;
-        $scope.textSelect = $scope.selectOptions[Number(organization.enableSms)].value;
+		// 'select' elements from dashboard options
+		$scope.feedbackSelect = $scope.selectOptions[Number(organization.enableFeedbacks)].value;
+		$scope.responseSelect = $scope.selectOptions[Number(organization.enableResponses)].value;
+		$scope.billSelect = $scope.selectOptions[Number(organization.enableBilling)].value;
+		$scope.textSelect = $scope.selectOptions[Number(organization.enableSms)].value;
 
-        // 'select' elements from voice call options
-        $scope.orderCancelSelect = $scope.selectOptions[Number(organization.enableOrderCancellation)].value;
-        $scope.broadcastEnableSelect = $scope.selectOptions[Number(organization.enableBroadcasts)].value;
-        
-        // 'checkbox' elements from incoming call settings
-        $scope.incomingCheckBoxOptions.order = organization.inboundCallAskOrder;
-        $scope.incomingCheckBoxOptions.feedback = organization.inboundCallAskFeedback;
-        $scope.incomingCheckBoxOptions.response = organization.inboundCallAskResponse;
+		// 'select' elements from voice call options
+		$scope.orderCancelSelect = $scope.selectOptions[Number(organization.enableOrderCancellation)].value;
+		$scope.broadcastEnableSelect = $scope.selectOptions[Number(organization.enableBroadcasts)].value;
+		
+		// 'checkbox' elements from incoming call settings
+		$scope.incomingCheckBoxOptions.order = organization.inboundCallAskOrder;
+		$scope.incomingCheckBoxOptions.feedback = organization.inboundCallAskFeedback;
+		$scope.incomingCheckBoxOptions.response = organization.inboundCallAskResponse;
 
-    });
-    
-    var outboundcall = OutboundCall.get({
-        id: outboundcallid
-    }, function() {
-    	
-    	//intialize 'checkbox' elements from outgoing call settings
-        $scope.outgoingCheckBoxOptions.order = outboundcall.askOrder;
-        $scope.outgoingCheckBoxOptions.feedback = outboundcall.askFeedback;
-        $scope.outgoingCheckBoxOptions.response = outboundcall.askResponse;
+	});
+	
+	var outboundcall = OutboundCall.get({
+		id: outboundcallid
+	}, function() {
+		
+		//intialize 'checkbox' elements from outgoing call settings
+		$scope.outgoingCheckBoxOptions.order = outboundcall.askOrder;
+		$scope.outgoingCheckBoxOptions.feedback = outboundcall.askFeedback;
+		$scope.outgoingCheckBoxOptions.response = outboundcall.askResponse;
 
-    });
-    
-    
+	});
+	
+	
 
-    // click function for 'save details' button in voice dashboard settings
-    $scope.updateDashboardOpt = function() {
+	// click function for 'save details' button in voice dashboard settings
+	$scope.updateDashboardOpt = function() {
 
-        $scope.organization = Organization.get({
-            id: orgid
-        }, function() {
+		$scope.organization = Organization.get({
+			id: orgid
+		}, function() {
 
-            //change the required attributes
-            $scope.organization.enableFeedbacks = Boolean(Number($scope.feedbackSelect));
-            $scope.organization.enableResponses = Boolean(Number($scope.responseSelect));
-            $scope.organization.enableBilling = Boolean(Number($scope.billSelect));
-            $scope.organization.enableOrderCancellation = Boolean(Number($scope.rejectSelect));
-            $scope.organization.enableSms = Boolean(Number($scope.textSelect));
+			//change the required attributes
+			$scope.organization.enableFeedbacks = Boolean(Number($scope.feedbackSelect));
+			$scope.organization.enableResponses = Boolean(Number($scope.responseSelect));
+			$scope.organization.enableBilling = Boolean(Number($scope.billSelect));
+			$scope.organization.enableOrderCancellation = Boolean(Number($scope.rejectSelect));
+			$scope.organization.enableSms = Boolean(Number($scope.textSelect));
 
-            //Finally , update the entity with required values
-            $scope.organization.$update({
-                id: orgid
-            }, function() {});
-        });
-    };
+			//Finally , update the entity with required values
+			$scope.organization.$update({
+				id: orgid
+			}, function() {});
+		});
+	};
 
-    $scope.updateVoiceCallOpt = function() {
+	$scope.updateVoiceCallOpt = function() {
 
-        $scope.organization = Organization.get({
-            id: orgid
-        }, function() {
+		$scope.organization = Organization.get({
+			id: orgid
+		}, function() {
 
-            //make changes in the $resource object
-            $scope.organization.enableFeedbacks = Boolean(Number($scope.feedbackSelect));
-            $scope.organization.enableResponses = Boolean(Number($scope.responseSelect));
-            $scope.organization.enableBilling = Boolean(Number($scope.billSelect));
-            $scope.organization.enableOrderCancellation = Boolean(Number($scope.rejectSelect));
-            $scope.organization.enableSms = Boolean(Number($scope.textSelect));
+			//make changes in the $resource object
+			$scope.organization.enableFeedbacks = Boolean(Number($scope.feedbackSelect));
+			$scope.organization.enableResponses = Boolean(Number($scope.responseSelect));
+			$scope.organization.enableBilling = Boolean(Number($scope.billSelect));
+			$scope.organization.enableOrderCancellation = Boolean(Number($scope.rejectSelect));
+			$scope.organization.enableSms = Boolean(Number($scope.textSelect));
 
-            //finally update the database
-            $scope.organization.$update({
-                id: orgid
-            }, function() {});
-        });
-    };
-    
-    // click function for 'save details' button in incoming call settings
-    $scope.updateIncomingCallOpt = function() {
+			//finally update the database
+			$scope.organization.$update({
+				id: orgid
+			}, function() {});
+		});
+	};
+	
+	// click function for 'save details' button in incoming call settings
+	$scope.updateIncomingCallOpt = function() {
 
-        $scope.organization = Organization.get({
-            id: orgid
-        }, function() {
+		$scope.organization = Organization.get({
+			id: orgid
+		}, function() {
 
-            //make changes in the $resource object
-            $scope.organization.inboundCallAskOrder = $scope.incomingCheckBoxOptions.order;
-            $scope.organization.inboundCallAskFeedback = $scope.incomingCheckBoxOptions.feedback;
-            $scope.organization.inboundCallAskResponse = $scope.incomingCheckBoxOptions.response;
+			//make changes in the $resource object
+			$scope.organization.inboundCallAskOrder = $scope.incomingCheckBoxOptions.order;
+			$scope.organization.inboundCallAskFeedback = $scope.incomingCheckBoxOptions.feedback;
+			$scope.organization.inboundCallAskResponse = $scope.incomingCheckBoxOptions.response;
 
-            //finally update the database
-            $scope.organization.$update({
-                id: orgid
-            }, function() {});
-        });
-    };
-    
-    // click function for 'save details' button in outgoing call settings
-    $scope.updateOutgoingCallOpt = function() {
+			//finally update the database
+			$scope.organization.$update({
+				id: orgid
+			}, function() {});
+		});
+	};
+	
+	// click function for 'save details' button in outgoing call settings
+	$scope.updateOutgoingCallOpt = function() {
 
-        $scope.outboundcall = OutboundCall.get({
-            id: orgid
-        }, function() {
+		$scope.outboundcall = OutboundCall.get({
+			id: orgid
+		}, function() {
 
-	        //make changes in the $resource object
-	        $scope.outboundcall.askOrder = $scope.outgoingCheckBoxOptions.order;
-	        $scope.outboundcall.askFeedback = $scope.outgoingCheckBoxOptions.feedback;
-	        $scope.outboundcall.askResponse = $scope.outgoingCheckBoxOptions.response;
-            
-            //finally update the database
-            $scope.outboundcall.$update({
-                id: orgid
-            }, function() {});
-        });
-    };
-    
-    $scope.uploadFile = function(){
-        var formData=new FormData();
-        formData.append("file",$scope.myFile); //myFile.files[0] will take the file and append in formData since the name is myFile.
-        $http({
-            method: 'POST',
-            url: '/web/iitb/upload/welcomeMessage', // The URL to Post.
-            headers: {'Content-Type': undefined}, // Set the Content-Type to undefined always.
-            data: formData,
-            transformRequest: function(data, headersGetterFunction) {
-                return data;
-            }
-        }).success(function(data, status) {  
-        })
-        .error(function(data, status) {
-        });
-    }
-    
-    
-    
+			//make changes in the $resource object
+			$scope.outboundcall.askOrder = $scope.outgoingCheckBoxOptions.order;
+			$scope.outboundcall.askFeedback = $scope.outgoingCheckBoxOptions.feedback;
+			$scope.outboundcall.askResponse = $scope.outgoingCheckBoxOptions.response;
+			
+			//finally update the database
+			$scope.outboundcall.$update({
+				id: orgid
+			}, function() {});
+		});
+	};
+	
+	$scope.uploadFile = function(){
+		
+		console.log("currently Selected value = " + $scope.WelcomeMessageLanguageSelect);
+		var localeIndex = $scope.WelcomeMessageLanguageSelect;
+		console.log("currently selected locale = " + $scope.languageOptions[localeIndex].locale);
+		
+		// We use formData to pass various attributes to the spring controller method
+		var formData=new FormData();
+		formData.append("file", $scope.myFile); //myFile.files[0] will take the file and append in formData since the name is myFile.
+		formData.append("locale", $scope.languageOptions[localeIndex].locale);
+		formData.append("orgid", orgid);
+		
+		$http({
+			method: 'POST',
+			url: '/web/iitb/upload/welcomeMessage', // The URL to Post.
+			headers: {'Content-Type': undefined}, // Set the Content-Type to undefined always.
+			data: formData,
+			transformRequest: function(data, headersGetterFunction) {
+				return data;
+			}
+		}).success(function(data, status) {  
+		})
+		.error(function(data, status) {
+		});
+	}
+	
+	
+	
 });
