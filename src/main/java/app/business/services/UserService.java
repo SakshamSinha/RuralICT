22ce8d2ec411278934f0d54d22d89433a633c026
Pranav Ganorkar <pranavg189@gmail.com>
2015-06-29 22:05:04 +0530

    Removed some manage-users branch unrelated files

--- src/main/java/app/business/services/UserService.java
+++ src/main/java/app/business/services/UserService.java
@@ -129,4 +129,6 @@ public class UserService {
 		
 		return userRepository.findAll();
 	}
+	
+	
 }
--- src/main/java/app/data/repositories/UserRepository.java
+++ src/main/java/app/data/repositories/UserRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
-import app.entities.Organization;
 import app.entities.User;
 
 @Repository
