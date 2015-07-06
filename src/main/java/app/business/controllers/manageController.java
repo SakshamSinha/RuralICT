052188a3ff54c5a347f9a94765bc6ef6ba7da766
abhijayghildyal <ghildyalabhijay92@gmail.com>
2015-07-06 15:50:17 +0530

    Inbound Call & manage members page-> show groups

---
+++ src/main/java/app/business/controllers/manageController.java
@@ -0,0 +1,27 @@
+package app.business.controllers;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import app.entities.message.Message;
+
+@Controller
+@RequestMapping("/web/{org}")
+public class manageController {
+
+	
+	@RequestMapping(value="/manage/incomingCalls/{groupId}")
+	@PreAuthorize("hasRole('ADMIN'+#org)")
+	@Transactional
+	public String manageIncomingCallsReports(@PathVariable String org, @PathVariable int groupId, Model model) {
+		return "manageInboundCallReports";
+	}
+
+}
