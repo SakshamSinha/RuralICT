66e3b7b26a4f55a3331304f0bca75a48d1b7f48b
Siddharth Ramachandran <siddharth.ramachandran2012@vit.ac.in>
2016-01-13 19:51:34 +0530

    GCM modifications

--- src/main/java/app/business/controllers/rest/GCMTestController.java
+++ src/main/java/app/business/controllers/rest/GCMTestController.java
@@ -1,8 +1,16 @@
 package app.business.controllers.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.google.android.gcm.server.Message;
+import com.google.android.gcm.server.MulticastResult;
+import com.google.android.gcm.server.Sender;
 
 @RestController
 @RequestMapping("/api")
@@ -11,11 +19,38 @@ import org.springframework.web.bind.annotation.RestController;
 public class GCMTestController {
 
 	@RequestMapping(value ="/broadcast", method = RequestMethod.GET )
-	public void broadcast(){
+	public void broadcast(@RequestParam (value="Message") String userMessage, @RequestParam(value="CollapseKey") String collapseKey, @RequestParam(value="Title") String title){
 		 final long serialVersionUID = 1L;
+		 List<String> androidTargets = new ArrayList<String>();
 		 final String SENDER_ID = "AIzaSyBsUr9b5VT5_oH_0FSygF8us4AXKA1yvGw";
-		 final String CHEAT = "APA91bEju-eB74DWRChlVt5gh7YfIVzNOr8gRYPisFbmcwBPlMJeGTYmdF7cYR3oL-F9KqmTey016drxmWAkYa4WQv9pQ_KvRzI1VUkql6ObbYGPkV7UBsm6pYoBw0dEk3veh60v3lVhDtLztWIbDc3XqtjU_fE_0g";
+		 final String CHEAT = "cfTzvcuWBxY:APA91bEj82KHgFMyKz-oT7SQoK_awFCP87hUza6G2tb4QTMAC2AC3CmKy9cGoGaGwCGH5NG0g_V3FK1bD6v-rBEzVS7gvLfvD7AB0BHFR-bWqdhg8HWj9giANBkuYRA6k0_tUHKPvLfQ";
+		 androidTargets.add(CHEAT);
+		 Sender sender = new Sender(SENDER_ID);
+	     Message message = new Message.Builder()
+	        .collapseKey(collapseKey)
+	        .timeToLive(30)
+	        .delayWhileIdle(true)
+	        .addData("message", userMessage)
+	        .addData("title", title)
+	        .build();
+	     
+	        try {
+
+	            MulticastResult result = sender.send(message, androidTargets, 1);
+	             
+	            if (result.getResults() != null) {
+	                int canonicalRegId = result.getCanonicalIds();
+	                if (canonicalRegId != 0) {
+	                     
+	                }
+	            } else {
+	                int error = result.getFailure();
+	                System.out.println("Broadcast failure: " + error);
+	            }
+	             
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        System.out.println("Done");
 	}
-	
-	
 }
